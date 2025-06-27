cd4ec7496f652187e1bb1d9f8fb85f678639786e
Pierre Hagelberg <pierre.hagelberg@gmail.com>
2025-06-27 16:08:38 +0200

    chore: config

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "images.unsplash.com",
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
       },
     ],
   },
-};
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
