a1a4e0a18d72a04653ed30fbc6afcf800a4dc914
Pierre Hagelberg <pierre.hagelberg@gmail.com>
2025-06-27 16:11:10 +0200

    chore: eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,18 @@
 module.exports = {
   root: true,
   extends: [
-    "next",
-    "next/core-web-vitals",
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
+    'next',
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
-  plugins: ["@typescript-eslint", "react-hooks"],
+  plugins: ['@typescript-eslint', 'react-hooks'],
   rules: {
-    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
-    "@typescript-eslint/no-explicit-any": "off",
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    semi: ['error', 'never'],
+    quotes: ['error', 'single'],
   },
-};
+}
